5b69430ba58f157ca019d59d4da0fc7a7086954d
ztana <peng.zuo.1990@gmail.com>
2020-06-27 14:30:17 +0800

    a

--- schema/schema.js
+++ schema/schema.js
@@ -40,7 +40,7 @@ const RootQuery = new GraphQLObjectType({
 			args:{name: {type: GraphQLString}},
 			resolve(parent, args) {
 				var n = '/'+args.name+'/';
-				return Book.find("name": {$regex: n});
+				return Book.find({"name": {$regex: n}});
 			}
 		},
 		books: {
